c4e41c4cedbdc0d64d123cec4b667321d3b4e12b
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2022-10-23 04:00:31 +0530

    added Custom Download

--- pages/components/toolbar/tools/download/Download.js
+++ pages/components/toolbar/tools/download/Download.js
@@ -4,29 +4,66 @@ import * as htmlToImage from "html-to-image";
 import { toPng, toJpeg, toBlob, toPixelData, toSvg } from "html-to-image";
 
 export default function Download() {
-  function downloadNormal() {
+  function downloadCustom(size) {
     document.getElementById("my-node").style.transform = "scale(1)"; //imp
+    let width = document.getElementById("parent-img").offsetWidth;
+    let height = document.getElementById("parent-img").offsetHeight;
     let style = { borderRadius: 0 };
     // JPG
     htmlToImage
-      .toJpeg(
-        document.getElementById("my-node"),
-        { quality: 1 },
-        {
-          style: style,
-        }
-      )
+      .toSvg(document.getElementById("my-node"), {
+        style: style,
+      })
       .then(function (dataUrl) {
         var link = document.createElement("a");
-        link.download = "spiffy.jpeg";
-        link.href = dataUrl;
-        link.click();
+        svgToPng(dataUrl, width, height, size / height).then((dataUri) => {
+          link.download = "spiffy.png";
+          link.href = dataUri;
+          link.click();
+        });
       });
   }
+  const svgToPng = (svgDataurl, width, height, scale) =>
+    new Promise((resolve, reject) => {
+      let canvas;
+      let ctx;
+      let img;
+
+      img = new Image();
+      img.src = svgDataurl;
+      img.onload = () => {
+        canvas = document.createElement("canvas");
+        canvas.width = img.width * scale;
+        canvas.height = img.height * scale;
+        ctx = canvas.getContext("2d");
+        ctx.drawImage(
+          img,
+          0,
+          0,
+          img.width,
+          img.height,
+          0,
+          0,
+          width * scale,
+          height * scale
+        );
+
+        img = new Image();
+        img.src = canvas.toDataURL("image/png");
+        img.onload = () => {
+          canvas = document.createElement("canvas");
+          canvas.width = width * scale;
+          canvas.height = height * scale;
+          ctx = canvas.getContext("2d");
+          ctx.drawImage(img, 0, 0);
+          resolve(canvas.toDataURL("image/png"));
+        };
+      };
+    });
   return (
     <button
       className="flex transform p-3 flex-col font-semibold text-xs justify-center items-center rounded-full bg-emerald-300 text-emerald-900 ease-in-out duration-200 hover:bg-emerald-300/90 group outline-none active:scale-95 ring ring-emerald-900 dark:ring-0"
-      onClick={downloadNormal}
+      onClick={() => downloadCustom(720)}
     >
       <ArrowDownTrayIcon className="w-7 stroke-[1.5]" />
       <span className="absolute -bottom-5 opacity-70 group-hover:opacity-100 dark:text-emerald-300 text-emerald-900">
